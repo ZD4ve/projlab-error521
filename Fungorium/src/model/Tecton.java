2f2ca0f9f2b2017e657facad232e76730e48ee25
marton200472 <marton200472@gmail.com>
2025-03-20 16:49:23 +0100

    tecton progress

--- Fungorium/src/model/Tecton.java
+++ Fungorium/src/model/Tecton.java
@@ -2,7 +2,10 @@ package model;
 
 import java.util.ArrayList;
 import java.util.Arrays;
+import java.util.HashSet;
+import java.util.LinkedList;
 import java.util.List;
+import java.util.Queue;
 
 public class Tecton implements IActive {
     private List<Tecton> neighbors;
@@ -16,6 +19,7 @@ public class Tecton implements IActive {
         mycelia = new ArrayList<>();
         spores = new ArrayList<>();
         insects = new ArrayList<>();
+        mushroom = null;
     }
 
     public void addNeighbor(Tecton tecton) {
@@ -55,12 +59,35 @@ public class Tecton implements IActive {
         return false;
     }
 
-    public boolean canGrowMyceliumFrom(Fungus fungus) {
+    public boolean canGrowMyceliumFrom(Fungus fungus) { // NOSONAR this param is needed in the specialized classes
         return true;
     }
 
     public int distanceTo(Tecton tecton) {
-        return 0;
+        if (tecton == this) {
+            return 0;
+        }
+        HashSet<Tecton> done = new HashSet<>();
+        Queue<Tecton> toVisit = new LinkedList<>();
+        done.add(this);
+        int dst = 1;
+        while (!toVisit.isEmpty()) {
+            HashSet<Tecton> nN = new HashSet<>();
+            for (Tecton t : toVisit) {
+                for (Tecton neighbor : t.neighbors) {
+                    if (neighbor == tecton) {
+                        return dst;
+                    }
+                    if (!done.contains(neighbor)) {
+                        done.add(neighbor);
+                        nN.add(neighbor);
+                    }
+                }
+            }
+            toVisit.addAll(nN);
+            dst++;
+        }
+        return Integer.MAX_VALUE;
     }
 
     public Tecton newMe() {
@@ -75,7 +102,8 @@ public class Tecton implements IActive {
 
     private boolean myceliumExists(Fungus fungus, Tecton t1, Tecton t2) {
         for (Mycelium m : mycelia) {
-            if (m.getSpecies() == fungus && (Arrays.equals(m.getEnds(), new Tecton[] {t1, t2})||Arrays.equals(m.getEnds(), new Tecton[] {t2, t1}))) {
+            if (m.getSpecies() == fungus && (Arrays.equals(m.getEnds(), new Tecton[] { t1, t2 })
+                    || Arrays.equals(m.getEnds(), new Tecton[] { t2, t1 }))) {
                 return true;
             }
         }
@@ -89,12 +117,35 @@ public class Tecton implements IActive {
         }
     }
 
+    public void tectonBreak() {
+        for (Tecton n : neighbors) {
+            n.removeNeighbor(this);
+        }
+        for (Mycelium m : mycelia) {
+            m.die();
+        }
+        var t1 = newMe();
+        var t2 = newMe();
+
+        var t1Neighbors = neighbors.subList(0, neighbors.size() / 2 - 1);
+        t1Neighbors.add(t2);
+        t1.fillWithStuff(spores.subList(0, spores.size() / 2 - 1),
+                mushroom, insects.subList(0, insects.size() / 2 - 1),
+                t1Neighbors);
+
+        var t2Neighbors = neighbors.subList(0, neighbors.size() / 2 - 1);
+        t2Neighbors.add(t1);
+        t2.fillWithStuff(spores.subList(spores.size() / 2, spores.size() - 1), null,
+                insects.subList(insects.size() / 2, insects.size() - 1),
+                t2Neighbors);
+    }
+
     @Override
     public void tick(double dT) {
         // TODO: logic to detemine if tecton is breaking
         boolean breaking = false;
         if (breaking) {
-            
+            tectonBreak();
         }
     }
 }
