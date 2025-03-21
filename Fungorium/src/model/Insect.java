3690a959cc15189433c40ac024f0da86ed2cc03f
ZDave <david@zoller.dev>
2025-03-21 14:10:35 +0100

    doc Insect

--- Fungorium/src/model/Insect.java
+++ Fungorium/src/model/Insect.java
@@ -5,21 +5,43 @@ import helper.Skeleton;
 import java.util.ArrayList;
 import java.util.List;
 
-// TODO: ASK hívások
+/**
+ * <h3>Rovar</h3>
+ * 
+ * Kezeli a felhasználó által kezdeményezett spóra evés, mozgás és fonal elrágás
+ * akciókat, illetve ezek során interaktál a többi objektummal. A rovar csak
+ * bizonyos időközönként végezhet akciókat, ezt is követi és ellenőrzi.
+ */
 public class Insect implements IActive {
-    private double cooldown;
-    private double speed = 1;
-    private int antiChewCount;
-    private boolean isParalysed;
-    private int score;
+    // ASSOCIATIONS
+    /** ezen a tektonon áll éppen a rovar. */
     private Tecton location;
+    /** épp a rovarra ható hatások */
     private final List<InsectEffect> activeEffects = new ArrayList<>();
+
+    // ATTRIBUTES
+    /** mennyi idő múlva végezhető a következő akció */
+    private double cooldown;
+    /** a rovar sebessége a normálishoz képest */
+    private double speed = 1;
+    /** hány darab fonalrágás tiltó hatás alatt van a rovar */
+    private int antiChewCount;
+    /** a rovar bénító hatás alatt van-e */
+    private boolean isParalysed;
+    /** hány spórát evett meg a rovar */
+    private int score;
 
     private static final double ACTION_DURATION = 1;
 
+    /**
+     * Rovar létrehozása és elhelyezése egy tektonon.
+     * 
+     * @param location a rovar kezdeti helye
+     */
     public Insect(Tecton location) {
         Skeleton.printCall(this.getClass(), List.of(location));
         this.location = location;
+        location.addInsect(this);
         Skeleton.printReturn(this);
     }
 
@@ -28,6 +50,18 @@ public class Insect implements IActive {
     private void setCooldown(double cooldown) {
         Skeleton.printCall(this, List.of(cooldown));
         this.cooldown = cooldown;
+        Skeleton.printReturn();
+    }
+
+    public double getSpeed() {
+        Skeleton.printCall(this);
+        Skeleton.printReturn(speed);
+        return speed;
+    }
+
+    public void setSpeed(double speed) {
+        Skeleton.printCall(this, List.of(speed));
+        this.speed = speed;
         Skeleton.printReturn();
     }
 
@@ -47,33 +81,6 @@ public class Insect implements IActive {
         Skeleton.printCall(this, List.of(isParalysed));
         this.isParalysed = isParalysed;
         Skeleton.printReturn();
-    }
-
-    public double getSpeed() {
-        Skeleton.printCall(this);
-        Skeleton.printReturn(speed);
-        return speed;
-    }
-
-    public void setSpeed(double speed) {
-        Skeleton.printCall(this, List.of(speed));
-        this.speed = speed;
-        Skeleton.printReturn();
-    }
-
-    // nem követi seq diagramot, mert az szar XD
-    public List<Tecton> getPotentialMoveTargets() {
-        Skeleton.printCall(this);
-        var ret = location.getNeighbors().stream().filter(t -> location.hasMyceliumTo(t)).toList();
-        Skeleton.printReturn(ret);
-        return ret;
-    }
-
-    public List<Mycelium> getPotentialChewTargets() {
-        Skeleton.printCall(this);
-        var ret = location.getMycelia();
-        Skeleton.printReturn(ret);
-        return ret;
     }
 
     public void addEffect(InsectEffect effect) {
@@ -103,12 +110,46 @@ public class Insect implements IActive {
 
     // #region FUNCTIONS
 
-    public void eatSpore() {
+    /**
+     * Megkeresi azokat a tektonokat, melyre a rovar képes átlépni, vagyis vezet oda
+     * gombafonal a jelenlegi pozíciójáról.
+     * 
+     * @return a lehetséges célpontok listája
+     */
+    public List<Tecton> getPotentialMoveTargets() {
         Skeleton.printCall(this);
+        // nem követi seq diagramot, mert az szar XD
+        var ret = location.getNeighbors().stream().filter(t -> location.hasMyceliumTo(t)).toList();
+        Skeleton.printReturn(ret);
+        return ret;
+    }
+
+    /**
+     * Megkeresi azokat a fonal, amelyeket a rovar elrághat.
+     * 
+     * @return a lehetséges célpontok listája
+     */
+    public List<Mycelium> getPotentialChewTargets() {
+        Skeleton.printCall(this);
+        var ret = location.getMycelia();
+        Skeleton.printReturn(ret);
+        return ret;
+    }
+
+    /**
+     * A rovar megpróbál elfogyasztani egy spórát azon a tektonon, amelyen áll. A
+     * visszatérési érték a művelet sikerességét jelzi.
+     * 
+     * @return sikeresség
+     */
+    public boolean eatSpore() {
+        Skeleton.printCall(this);
+        boolean success = false;
         if (!isParalysed && ready()) {
             Spore sporeTaken = location.takeSpore();
             if (sporeTaken != null) {
                 score++;
+                success = true;
                 InsectEffect effect = sporeTaken.getEffect();
                 if (effect != null) {
                     effect.applyTo(this);
@@ -116,33 +157,52 @@ public class Insect implements IActive {
                 setCooldown(ACTION_DURATION);
             }
         }
-        Skeleton.printReturn();
+        Skeleton.printReturn(success);
+        return success;
     }
 
-    public void moveTo(Tecton target) {
+    /**
+     * A rovar megpróbál a target tektonra mozogni. A visszatérési érték a művelet
+     * sikerességét jelzi.
+     * 
+     * @param target cél tekton
+     * @return sikeresség
+     */
+    public boolean moveTo(Tecton target) {
         Skeleton.printCall(this, List.of(target));
+        boolean success = false;
         if (!isParalysed && ready()) {
             boolean moveValid = location.hasMyceliumTo(target);
             if (moveValid) {
+                success = true;
                 location.removeInsect(this);
                 location = target;
                 location.addInsect(this);
                 setCooldown(ACTION_DURATION);
             }
         }
-        Skeleton.printReturn();
+        Skeleton.printReturn(success);
+        return success;
     }
 
-    public void chewMycelium(Mycelium mycelium) {
+    /**
+     * A rovar megpróbálja elrágni a target gombafonalat. A visszatérési érték a
+     * művelet sikerességét jelzi.
+     * 
+     * @param mycelium a fonal, amit el akar rágni
+     * @return sikeresség
+     */
+    public boolean chewMycelium(Mycelium mycelium) {
         Skeleton.printCall(this, List.of(mycelium));
         if (isParalysed || antiChewCount > 0 || !ready()) {
-            Skeleton.printReturn();
-            return;
+            Skeleton.printReturn(false);
+            return false;
         }
         // TODO: check mycelium valid-e, seq-en nem szerepel
         mycelium.die();
         setCooldown(ACTION_DURATION);
-        Skeleton.printReturn();
+        Skeleton.printReturn(true);
+        return true;
     }
 
     @Override
