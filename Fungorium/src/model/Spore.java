7bddee99b0b8b0f2bff1ddba94ab2eaba7e70df5
ZDave <david@zoller.dev>
2025-03-22 20:42:51 +0100

    Spore implemented

--- Fungorium/src/model/Spore.java
+++ Fungorium/src/model/Spore.java
@@ -1,17 +1,29 @@
 package model;
 
+import java.util.List;
+
+import helper.Skeleton;
+
 public class Spore {
     private Fungus species;
 
     public Spore(Fungus fungus) {
+        Skeleton.printCall(this.getClass(), List.of(fungus));
         this.species = fungus;
+        Skeleton.printReturn(this);
     }
 
     public InsectEffect getEffect() {
-        return null;
+        Skeleton.printCall(this);
+        InsectEffect effect = InsectEffect.createEffect();
+        Skeleton.printReturn(effect);
+        return effect;
+
     }
 
     public Fungus getSpecies() {
+        Skeleton.printCall(this);
+        Skeleton.printReturn(species);
         return species;
     }
 }
