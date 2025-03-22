63c3181d318e96e68bc6989dc0a8783eb5e727bd
marton200472 <marton200472@gmail.com>
2025-03-22 22:12:48 +0100

    remove return this

--- Fungorium/src/model/Mushroom.java
+++ Fungorium/src/model/Mushroom.java
@@ -23,33 +23,30 @@ public class Mushroom implements IActive {
         Skeleton.printCall(this, List.of(target));
         boolean isReady = Skeleton.ask("Készen áll m1 a spóra szórására?");
         int distance = location.distanceTo(target);
-        
-        if(isReady)
-        {
-            if(distance == 2)
-            {
+
+        if (isReady) {
+            if (distance == 2) {
                 boolean isGrown = Skeleton.ask("Fejlett-e a gomba?");
-                if(isGrown) range = 2;
+                if (isGrown)
+                    range = 2;
             }
-            if(distance<=range)
-                {
-                    Spore spo = new Spore(species);
-                    Skeleton.addObject(spo, "spo");
-                    target.addSpore(spo);
-                    boolean alive = Skeleton.ask("Tud még spórát szórni a gomba?");
-                    if(!alive)
-                    {
-                        location.removeMushroom();
-                        species.removeMushroom(this);
-                    }
+            if (distance <= range) {
+                Spore spo = new Spore(species);
+                Skeleton.addObject(spo, "spo");
+                target.addSpore(spo);
+                boolean alive = Skeleton.ask("Tud még spórát szórni a gomba?");
+                if (!alive) {
+                    location.removeMushroom();
+                    species.removeMushroom(this);
                 }
+            }
         }
-        Skeleton.printReturn(this);
+        Skeleton.printReturn();
     }
 
     @Override
     public void tick(double dT) {
-        
+
     }
 
     public Fungus getSpecies() {
