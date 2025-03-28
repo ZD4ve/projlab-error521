2725a60e57f3114b61d3953248fa085393274e89
Scumbush <vazulvargalaboczki@gmail.com>
2025-03-28 13:25:36 +0100

    unfished proto

--- Fungorium/src/proto/Proto.java
+++ Fungorium/src/proto/Proto.java
@@ -1,5 +1,7 @@
 package proto;
 
+import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.HashMap;
 
 public class Proto {
@@ -9,42 +11,64 @@ public class Proto {
 
     }
 
+    private static void tectonsMenu(String[] params) {
+
+    }
+
+    private static void neighborsMenu(String[] params) {
+
+    }
+
+    private static void mushroomsMenu(String[] params) {
+
+    }
+
+    private static void myceliaMenu(String[] params) {
+
+    }
+
+    private static void sporesMenu(String[] params) {
+
+    }
+
+    // Main menu
     private static void buildPhase() {
         String input;
-
-        while (true) {
+        
+        do {
             input = System.console().readLine();
-            if (input.length() == 0)
+            if (input.isEmpty())
                 continue;
-            if (input.toLowerCase().endsWith("end"))
-                break;
 
-            String[] tokens = input.split(" ");
-
-            switch (tokens[0].toLowerCase()) {
-            case "tectons":
-                // Tekton létrehozása almenü
-                break;
-            case "neighbors":
-                // Szomszédok hozzáadása
-                break;
-            case "fungi":
-                // Gombafajok hozzáadása
-                break;
-            case "colonies":
-                // kolóniák hozzáadása
-                break;
-            case "mushrooms":
-                // gombatestek hozzáadása
-                break;
-            case "mycelia":
-                // gombatestek hozzáadása
-                break;
-            default:
-                System.out.println("Invalid command");
-
-            }
-        }
+            var tokens = new ArrayList<String>(Arrays.asList(input.split(" ")));
+            switch (tokens.get(0).toLowerCase()) {
+                    case "tectons":
+                        // Tekton létrehozása almenü
+                        tectonsMenu(tokens.subList(1, tokens.size()).toArray(new String[0]));
+                        break;
+                    case "neighbors":
+                        // Szomszédok hozzáadása
+                        neighborsMenu(tokens.subList(1, tokens.size()).toArray(new String[0]));
+                        break;
+                    case "fungi":
+                        // Gombafajok hozzáadása
+                        break;
+                    case "colonies":
+                        // kolóniák hozzáadása
+                        break;
+                    case "mushrooms":
+                        // gombatestek hozzáadása
+                        break;
+                    case "mycelia":
+                        // gombatestek hozzáadása
+                        break;
+                    case "insects":
+                        // rovarok hozzáadása
+                        break;
+                    default:
+                        System.out.println("Invalid command");
+                }
+        } while (!input.toLowerCase().endsWith("end"));
     }
 
     // there is no static class in java
