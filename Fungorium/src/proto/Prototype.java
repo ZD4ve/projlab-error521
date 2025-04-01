bc46cfc325f970c72beab77ef9e15b4c16ef7bab
marton200472 <marton200472@gmail.com>
2025-04-01 22:39:36 +0200

    fix faulty max

--- Fungorium/src/proto/Prototype.java
+++ Fungorium/src/proto/Prototype.java
@@ -20,10 +20,9 @@ public class Prototype {
             ParalysingEffect.class, "para", SpeedEffect.class, "sped");
 
     public static void registerNamedObject(Class cls, Object obj) {
-        namedObjects.put(
-                String.format("%s%02d", Prototype.names.get(cls),
-                        namedObjects.keySet().stream().filter(x -> x.startsWith(Prototype.names.get(cls)))
-                                .map(x -> Integer.parseInt(x.substring(2))).max((x, y) -> x > y ? 1 : 0).orElse(0) + 1),
+        namedObjects.put(String.format("%s%02d", Prototype.names.get(cls),
+                namedObjects.keySet().stream().filter(x -> x.startsWith(Prototype.names.get(cls)))
+                        .mapToInt(x -> Integer.parseInt(x.substring(2))).max().orElse(0) + 1),
                 obj);
     }
 
