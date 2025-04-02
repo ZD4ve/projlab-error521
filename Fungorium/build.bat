85a2f1bc30ef51e74d748c789db83e7b8c4832f1
ZDave <david@zoller.dev>
2025-04-02 20:59:36 +0200

    fix2

--- Fungorium/build.bat
+++ Fungorium/build.bat
@@ -1,7 +1,12 @@
 @echo off
 if not exist bin mkdir bin
 dir /s /B *.java | findstr /V "src\tester" > sources.txt
-for /f "delims=" %%f in (sources.txt) do echo "%%f" >> quoted_sources.txt
+for /f "delims=" %%f in (sources.txt) do (
+    set "line=%%f"
+    setlocal enabledelayedexpansion
+    echo "!line:\=\\!" >> quoted_sources.txt
+    endlocal
+)
 del sources.txt
 javac -d bin @quoted_sources.txt
 del quoted_sources.txt
