072b0f60a5744e1da2d7de17394236d5fea3ac5d
ZDave <david@zoller.dev>
2025-04-02 20:57:31 +0200

    fix

--- Fungorium/build.bat
+++ Fungorium/build.bat
@@ -1,10 +1,12 @@
 @echo off
 if not exist bin mkdir bin
 dir /s /B *.java | findstr /V "src\tester" > sources.txt
-javac -d bin @sources.txt
+for /f "delims=" %%f in (sources.txt) do echo "%%f" >> quoted_sources.txt
+del sources.txt
+javac -d bin @quoted_sources.txt
+del quoted_sources.txt
 if errorlevel 1 (
     echo Compilation failed.
     exit /b 1
 )
 jar cfe Fungorium.jar proto.Prototype -C bin .
-del sources.txt
