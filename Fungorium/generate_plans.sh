219d0e3a17d194138d50bbffa474c2682409ccfd
ZDave <david@zoller.dev>
2025-04-04 15:51:55 +0200

    fix

--- Fungorium/generate_plans.sh
+++ Fungorium/generate_plans.sh
@@ -8,5 +8,5 @@ doxygen
 sed -E 's/^(.*)\{\\f2 \[private\]\}/- \1/' rtf/refman.rtf | \
 sed -E 's/^(.*)\{\\f2 \[protected\]\}/# \1/' | \
 sed -E 's/^(.*)\{\\f2 \[static\]\}/\\ul\n\1/' | \
-sed -E 's/^([a-zA-Z][^\n]*\([^\n]*\)\}\})/+ \1/' > "$output_file"
+sed -E 's/^([a-zA-Z{][^\n]*\([^\n]*\)\}\})/+\1/' > "$output_file"
 rm -r rtf
