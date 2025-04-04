500a847538124b9916b4cfda20d24f4e51659ab6
ZDave <david@zoller.dev>
2025-04-04 15:44:38 +0200

    add + to public functions

--- Fungorium/generate_plans.sh
+++ Fungorium/generate_plans.sh
@@ -5,8 +5,8 @@ set -e
 output_file="plans.rtf"
 
 doxygen
-sed -E 's/^(.*)\{\\f2 \[private\]\}/- \1/' "rtf/refman.rtf" > "tmp1"
-sed -E 's/^(.*)\{\\f2 \[protected\]\}/# \1/' "tmp1" > "tmp2"
-sed -E 's/^(.*)\{\\f2 \[static\]\}/\\ul\n\1/' "tmp2" > "$output_file"
-rm "tmp1" "tmp2"
+sed -E 's/^(.*)\{\\f2 \[private\]\}/- \1/' rtf/refman.rtf | \
+sed -E 's/^(.*)\{\\f2 \[protected\]\}/# \1/' | \
+sed -E 's/^(.*)\{\\f2 \[static\]\}/\\ul\n\1/' | \
+sed -E 's/^([a-zA-Z][^\n]*\([^\n]*\)\}\})/+ \1/' > "$output_file"
 rm -r rtf
