a358e6d8c42fd201a8231559403dcbf4360d146a
marton200472 <marton200472@gmail.com>
2025-04-07 01:05:09 +0200

    now created stuff should get names and stuff :)

--- Fungorium/src/controller/Controller.java
+++ Fungorium/src/controller/Controller.java
@@ -9,16 +9,26 @@ public class Controller {
     private static List<IActive> activeObjects = new ArrayList<>();
     private static List<Tecton> tectons = new ArrayList<>();
 
-    private static IActive lastRemoved = null;
+    private static List<ObjectEventHandler> objectEventHandlers = new ArrayList<>();
+
+    private static Set<IActive> removedStuff = new HashSet<>();
 
     public static void registerTecton(Tecton t) {
         if (!tectons.contains(t)) {
             tectons.add(t);
+
         }
     }
 
     public static void unregisterTecton(Tecton t) {
         tectons.remove(t);
+        for (var handler : objectEventHandlers) {
+            handler.handle(t, false);
+        }
+    }
+
+    public static void addTectonEventHandler(ObjectEventHandler handler) {
+        objectEventHandlers.add(handler);
     }
 
     // #region IActive related stuff
@@ -26,20 +36,25 @@ public class Controller {
     public static void registerActiveObject(IActive object) {
         if (!activeObjects.contains(object)) {
             activeObjects.add(object);
+            for (var handler : objectEventHandlers) {
+                handler.handle(object, true);
+            }
         }
     }
 
     public static void unregisterActiveObject(IActive object) {
-        lastRemoved = object;
+        removedStuff.add(object);
         activeObjects.remove(object);
+        for (var handler : objectEventHandlers) {
+            handler.handle(object, false);
+        }
     }
 
     public static void onTimeElapsed(double dT) {
-        for (int i = 0; i < activeObjects.size();) {
-            var curr = activeObjects.get(i);
-            curr.tick(dT);
-            if (lastRemoved != curr) {
-                i++;
+        var aOCopy = activeObjects.stream().toList();
+        for (var e : aOCopy) {
+            if (!removedStuff.contains(e)) {
+                e.tick(dT);
             }
         }
     }
