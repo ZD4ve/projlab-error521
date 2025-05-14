61e9a13eaef84cd2fd810f20393be0794038deaf
marton200472 <marton200472@gmail.com>
2025-05-14 22:04:48 +0200

    refactor create

--- Fungorium/src/view/View.java
+++ Fungorium/src/view/View.java
@@ -9,7 +9,6 @@ import java.util.HashSet;
 import java.util.List;
 import java.util.Objects;
 import java.util.Set;
-import java.util.stream.Collectors;
 import java.util.stream.IntStream;
 
 import controller.RandomProvider;
@@ -17,32 +16,33 @@ import model.*;
 
 class Point {
     public Point(int x, int y) {
-        X = x;
-        Y = y;
+        this.x = x;
+        this.y = y;
     }
 
-    public int X, Y;
-
-    static Point random(int xbound, int ybound) {
-        return new Point(RandomProvider.nextInt(xbound), RandomProvider.nextInt(ybound));
-    }
+    int x;
+    int y;
 
     public double distanceTo(Point p) {
-        return Math.sqrt(Math.pow(X - p.X, 2) + Math.pow(Y - p.Y, 2));
+        return Math.sqrt(Math.pow((x - p.x), 2) + Math.pow((y - p.y), 2));
     }
 
     @Override
     public int hashCode() {
-        return Objects.hash(X, Y);
+        return Objects.hash(x, y);
     }
 
     @Override
     public boolean equals(Object obj) {
         if (obj instanceof Point other) {
-            return X == other.X && Y == other.Y;
+            return x == other.x && y == other.y;
         }
         return false;
     }
+}
+
+interface GameElementCreator<T> {
+    void create(Cell cell, T instance);
 }
 
 public class View {
@@ -54,8 +54,8 @@ public class View {
     private static VPlayer selectedPlayer;
     private static List<VColony> allColonies;
     private static List<VFungus> allFungi;
-    private static BufferedImage canvas = new BufferedImage(600, 600, BufferedImage.TYPE_INT_ARGB); // TODO @Panni not
-                                                                                                    // hardcoded w/h
+    private static BufferedImage canvas = new BufferedImage(1500, 1000, BufferedImage.TYPE_INT_ARGB); // TODO @Panni not
+                                                                                                      // hardcoded w/h
     private static Color backgroundColor = new Color(60, 120, 216, 255);
 
     public static void redraw() {
@@ -69,78 +69,90 @@ public class View {
         g.dispose();
     }
 
-    // TODO tecNum must be greater than fungiNum + colNum
-    public static void create(int tecNum, int fungiNum, int colNum) {
-
-        allFungi = IntStream.range(0, fungiNum).mapToObj(x -> new VFungus(new Fungus())).collect(Collectors.toList());
-        allColonies = IntStream.range(0, colNum).mapToObj(x -> new VColony(new Colony())).collect(Collectors.toList());
-
-        final int minDst = 3;
-        final int rows = (int) Math.ceil(1.5 * Math.sqrt(tecNum * minDst * minDst)), cols = rows;
-
-        final int mrows = rows / minDst, mcols = cols / minDst;
-
+    private static Set<Point> createControlPoints(int tecNum, int minUnitCols, int minUnitRows, int minDst) {
         Set<Point> controlPoints = new HashSet<>();
         for (int i = 0; i < tecNum; i++) {
             Point p;
             do {
-                p = Point.random(mcols, mrows);
-                p.X = p.X * minDst + 1;
-                p.Y = p.Y * minDst + 1;
+                p = new Point(RandomProvider.nextInt(minUnitCols) * minDst + 2,
+                        RandomProvider.nextInt(minUnitRows) * minDst + 2);
             } while (controlPoints.contains(p));
             controlPoints.add(p);
         }
+        return controlPoints;
+    }
 
-        map = new Map(cols, rows, canvas.getWidth() / cols);
+    private static HashMap<Point, List<Cell>> calculateTectonCells(final int rows, final int cols,
+            Set<Point> controlPoints) {
         HashMap<Point, List<Cell>> tectonsCells = new HashMap<>();
         for (int i = 0; i < cols; i++) {
             for (int j = 0; j < rows; j++) {
                 Point curr = new Point(i, j);
                 Point closest = controlPoints.stream()
-                        .sorted((a, b) -> Double.compare(a.distanceTo(curr), b.distanceTo(curr))).findFirst().get();
+                        .sorted((a, b) -> Double.compare(a.distanceTo(curr), b.distanceTo(curr))).findFirst()
+                        .orElse(null);
                 tectonsCells.putIfAbsent(closest, new ArrayList<>());
                 tectonsCells.get(closest).add(map.getCellByIndex(i, j));
             }
         }
+        return tectonsCells;
+    }
+
+    private static Tecton createRandomTypeTecton() {
+        Tecton tecton;
+        double rand = RandomProvider.nextRand();
+        if (rand < 0.8) {
+            tecton = new Tecton();
+        } else if (rand < 0.85) {
+            tecton = new MyceliumAbsorbingTecton();
+        } else if (rand < 0.9) {
+            tecton = new SingleMyceliumTecton();
+        } else if (rand < 0.95) {
+            tecton = new NoMushroomTecton();
+        } else {
+            tecton = new MyceliumKeepingTecton();
+        }
+        return tecton;
+    }
+
+    private static <T> void putStuffOnEmptyTectons(List<VTecton> emptyVTectons, List<T> all,
+            GameElementCreator<T> creator) {
+        for (T f : all) {
+            VTecton targetTecton;
+            targetTecton = emptyVTectons.remove(RandomProvider.nextInt(emptyVTectons.size()));
+            Cell targetCell = targetTecton.getCells().get(RandomProvider.nextInt(targetTecton.getCells().size()));
+            creator.create(targetCell, f);
+        }
+    }
+
+    // TODO tecNum must be greater than fungiNum + colNum @Tamasz
+    public static void create(int tecNum, int fungiNum, int colNum) {
+        allFungi = IntStream.range(0, fungiNum).mapToObj(x -> new VFungus(new Fungus())).toList();
+        allColonies = IntStream.range(0, colNum).mapToObj(x -> new VColony(new Colony())).toList();
+
+        final int minDst = 3;
+        final int rows = (int) Math.ceil(4 * Math.sqrt((tecNum * minDst * minDst)));
+        final int cols = 3 * rows / 2;
+
+        final int minUnitRows = rows / minDst;
+        final int minUnitCols = cols / minDst;
+
+        Set<Point> controlPoints = createControlPoints(tecNum, minUnitCols, minUnitRows, minDst);
+
+        map = new Map(cols, rows, canvas.getWidth() / cols);
+
+        HashMap<Point, List<Cell>> tectonsCells = calculateTectonCells(rows, cols, controlPoints);
 
         List<VTecton> vtectons = new ArrayList<>();
         for (var entry : tectonsCells.entrySet()) {
-            Tecton tecton;
-            double rand = RandomProvider.nextRand();
-            if (rand < 0.2) {
-                tecton = new Tecton();
-            } else if (rand < 0.4) {
-                tecton = new MyceliumAbsorbingTecton();
-            } else if (rand < 0.6) {
-                tecton = new SingleMyceliumTecton();
-            } else if (rand < 0.8) {
-                tecton = new NoMushroomTecton();
-            } else {
-                tecton = new MyceliumKeepingTecton();
-            }
-            vtectons.add(new VTecton(entry.getValue(), tecton));
+            vtectons.add(new VTecton(entry.getValue(), createRandomTypeTecton()));
         }
 
-        for (VFungus f : allFungi) {
-            VTecton targetTecton;
-            do {
-                targetTecton = vtectons.get(RandomProvider.nextInt(tecNum));
-            } while (targetTecton.getTecton().getMushroom() != null
-                    || !targetTecton.getTecton().getInsects().isEmpty());
+        putStuffOnEmptyTectons(vtectons, allFungi,
+                (cell, vf) -> new VMushroom(cell, new Mushroom(vf.getFungus(), cell.getTecton().getTecton())));
 
-            Cell targetCell = targetTecton.getCells().get(RandomProvider.nextInt(targetTecton.getCells().size()));
-            new VMushroom(targetCell, new Mushroom(f.getFungus(), targetTecton.getTecton()));
-        }
-
-        for (VColony c : allColonies) {
-            VTecton targetTecton;
-            do {
-                targetTecton = vtectons.get(RandomProvider.nextInt(tecNum));
-            } while (targetTecton.getTecton().getMushroom() != null
-                    || !targetTecton.getTecton().getInsects().isEmpty());
-            Cell targetCell = targetTecton.getCells().get(RandomProvider.nextInt(targetTecton.getCells().size()));
-            new VInsect(targetCell, new Insect(targetTecton.getTecton(), c.getColony()));
-        }
+        putStuffOnEmptyTectons(vtectons, allColonies,
+                (cell, vf) -> new VInsect(cell, new Insect(cell.getTecton().getTecton(), vf.getColony())));
 
         // for testing:
         /*
