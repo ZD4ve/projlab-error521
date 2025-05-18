c8e87b62f55582a0bffcc8a57a74a4b6dfb46703
Szabó Tamás János <tomiszabo2004@gmail.com>
2025-05-18 22:31:43 +0200

    ColorPanel docs

--- Fungorium/src/gui/ColorPanel.java
+++ Fungorium/src/gui/ColorPanel.java
@@ -7,13 +7,31 @@ import java.util.List;
 import javax.swing.*;
 import view.VPlayer;
 
+/**
+ * <h3>ColorPanel</h3>
+ * 
+ * A játékosokhoz tartozó színes téglalap, valamint pontszám megjelenítéséért felelő osztály.
+ */
 public class ColorPanel extends JPanel {
 
+    // #region ATTRIBUTES
+    /** A játékos színét tartalmazó panel. */
     JPanel colorBox;
+    /** A játékos pontszámát megjelenítő label. */
     JLabel label;
+    /** Az a játékos, amelyhez tartozó ColorPanelt készítjük. */
     transient VPlayer player;
+    /** A ColorPanelek példányait tároló lista. */
     static final List<ColorPanel> instances = new ArrayList<>();
+    // #endregion
 
+    // #region CONSTRUCTOR
+    /**
+     * Konstruktor, amely létrehozza a ColorPanelt. Beállítja a panel színét és a pontszámot. Kezeli a játékos
+     * választását gombnyomáskor.
+     * 
+     * @param player Az a játékos, amelyhez a ColorPanel tartozik.
+     */
     public ColorPanel(VPlayer player) {
         super(new FlowLayout(FlowLayout.LEFT, 10, 0));
         setAlignmentX(Component.LEFT_ALIGNMENT);
@@ -40,15 +58,20 @@ public class ColorPanel extends JPanel {
         });
     }
 
+    // #region FUNCTIONS
+    /** Frissíti a ColorPanelek tartalmát. */
     public static void updateAll() {
         for (ColorPanel cp : instances) {
             cp.update();
         }
     }
 
+    /** A Pontszámok frissítését, valamint a kiválasztott panel frissítését megvalósító metódus. */
     private void update() {
         label.setText("pont: " + player.getScore());
         colorBox.setBorder(player.isSelected() ? BorderFactory.createMatteBorder(3, 3, 3, 3, Color.orange)
                 : BorderFactory.createEmptyBorder());
     }
+
+    // #endregion
 }
