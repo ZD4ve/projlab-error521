ad699bb07bf3054b9dad58909ade92fe8d9d3c90
Szabó Tamás János <tomiszabo2004@gmail.com>
2025-05-18 22:04:09 +0200

    comment

--- Fungorium/src/gui/Main.java
+++ Fungorium/src/gui/Main.java
@@ -2,7 +2,14 @@ package gui;
 
 import javax.swing.*;
 
+/**
+ * A grafikus alkalmazás főosztálya.
+ */
 public class Main {
+
+    /**
+     * Fő metódus, amely megnyitja a grafikus alkalmazás kezdőablakát.
+     */
     public static void main(String[] args) {
         JFrame mf = new StartFrame();
         mf.pack();
